4884342531689ae4129f142b57f68388f420ed01
Jay <vijayasimhabr@live.com>
2024-10-03 12:24:16 +0530

    ongoing code
    ongoing code

---
+++ SimpleCalculator/SimpleCalculatorRazorMVCWebApp/SimpleCalculatorRazorMVCWebApp/DTO/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleCalculatorRazorMVCWebApp.DTO
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool? Success { get; set; } = true;  //I will assume that, by default, everything went fine.
+        public string? Message { get; set; } = ""; //by default, there is nothing to report. 
+    }
+}
